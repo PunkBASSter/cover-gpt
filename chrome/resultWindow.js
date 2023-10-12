5bc71d62e0cbc22026a2669df591c4eb93a7f690
Andrei <stdrew90@gmail.com>
2023-10-12 17:29:58 +0200

    Added copied btn hint

--- chrome/resultWindow.js
+++ chrome/resultWindow.js
@@ -12,10 +12,16 @@ document.getElementById('copyButton').addEventListener('click', function() {
 
 function copyTextToClipboard(text) {
     navigator.clipboard.writeText(text)
-    .then(() => {
-      console.log('Text copied to clipboard');
-    })
-    .catch(err => {
-      console.error('Failed to copy text: ', err);
-    });
-}
+      .then(() => {
+        const tooltip = document.getElementById('tooltip');
+        tooltip.classList.remove('hidden');
+        
+        setTimeout(() => {
+          tooltip.classList.add('hidden');
+        }, 1000);
+      })
+      .catch(err => {
+        console.error('Failed to copy text: ', err);
+      });
+  }
+  
